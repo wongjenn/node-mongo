7c3beb5555fb8641cb8ab1ed3fd2384e154d15a4
wongjenn <wongjenn@yahoo.com>
2017-03-21 15:43:11 -0400

    done

--- server.js
+++ server.js
@@ -4,85 +4,33 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); // req.body
 const cors = require('cors');
+const mongoose = require("mongoose");
+const uriUtil = require("mongodb-uri");
 let contacts = require('./data');
+
+const mongodbUri = 'mongodb://nodeMongoScotch:secret@ds137090.mlab.com:37090/node-mongo-scotch';
+const mongooseUri = uriUtil.formatMongoose(mongodbUri);
+const dbOptions = {};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/contacts', (request, response) => {
-  if (!contacts) {
-    response.status(404).json({ message: 'No contacts found' });
-  }
-  response.json(contacts);
-});
-
-app.get('/api/contacts/:id', (request, response) => {
-  const requestId = request.params.id;
-
-  let contact = contacts.filter( contact => {
-    return contact.id == requestId;
-  })
-
-  if (contact.length === 0) {
-    response.status(404).json({ message: 'No contact found' });
-  }
-
-  response.json(contact[0]);
-});
-
-app.post('/api/contacts', (request, response) => {
-  const contact = {
-    id: contacts.length + 1,
-    first_name: request.body.first_name,
-    last_name: request.body.last_name,
-    email: request.body.email,
-    website: request.body.website
-  }
-
-  contacts.push(contact);
-  response.json(contact);
-});
-
-app.post('/api/contacts/:id', (request, response) => {
-  const requestId = request.params.id;
-
-  let contact = contacts.filter( contact => {
-    return contact.id == requestId;
-  })[0];
-
-  const index = contacts.indexOf(contact);
-
-  const keys = Object.keys(request.body);
-
-  keys.forEach( key => {
-    contact[key] = request.body[key];
-  });
-
-  response.json(contacts[index]);
-})
-
-// GET, POST, PUT, DELETE, PATCH
-
-app.delete('/api/contacts/:id', (request, response) => {
-  const requestId = request.params.id;
-
-  let contact = contacts.filter( contact => {
-    return contact.id == requestId;
-  })[0];
-
-  const index = contacts.indexOf(contact);
-
-  contacts.splice(index, 1);
-
-  response.json({ message: `User ${requestId} deleted.` });
-})
+app.use('/api/contacts', require('./api/contacts/routes/post_contacts.js'));
+app.use('/api/contacts', require('./api/contacts/routes/get_contacts.js'));
+app.use('/api/contacts', require('./api/contacts/routes/get_contact.js'));
+app.use('/api/contacts', require('./api/contacts/routes/delete_contact.js'));
+app.use('/api/contacts', require('./api/contacts/routes/put_contact.js'));
 
 const hostname = 'localhost';
 const port = 3001;
 
-
-
 app.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}`);
+  mongoose.connect(mongooseUri, dbOptions, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Server is running at http://${hostname}:${port}`);
+  })
+
 });
